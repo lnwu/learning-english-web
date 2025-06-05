e3b88c9499a6cb01435e94b3f708e8035df2e095
lnwu <wind2729@gmail.com>
2025-06-05 16:50:04 +0800

    disable refresh before all words correct

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,85 @@
 "use client";
 
 import { Input } from "@/components/ui/input";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useLocalStorage } from "react-use";
-import { observer, useLocalObservable } from "mobx-react-lite";
+import { observer } from "mobx-react-lite";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { makeAutoObservable } from "mobx";
+
+const MAX_RANDOM_WORDS = 5;
+
+class Words {
+  randomWords: Map<string, string> = new Map();
+  pendingWords: Map<string, string> = new Map();
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  setRandomWords(words: [string, string][]) {
+    this.pendingWords.clear();
+    this.randomWords = new Map(words);
+  }
+
+  setPendingWord(word: string, value: string) {
+    this.pendingWords.set(word, value);
+  }
+
+  get correct() {
+    return Array.from(this.randomWords).every(([word]) => {
+      const pendingValue = this.pendingWords.get(word);
+      return pendingValue && pendingValue.toLowerCase() === word.toLowerCase();
+    });
+  }
+}
+
+const words = new Words();
 
 const Home = observer(() => {
   const [allWords] = useLocalStorage<[string, string][]>("words");
-  const [randomWords, setRandomWords] = useState<[string, string][]>([]);
   const [isClient, setIsClient] = useState(false);
 
-  const inputWords = useLocalObservable(() => ({
-    words: new Map<string, string>(),
-    set(key: string, value: string) {
-      this.words.set(key, value);
-    },
-  }));
+  const getRandomWords = useCallback(() => {
+    if (allWords && allWords.length > 0) {
+      const shuffled = allWords.sort(() => 0.5 - Math.random());
+      return shuffled.slice(0, MAX_RANDOM_WORDS);
+    }
+    return [];
+  }, [allWords]);
 
   useEffect(() => {
     setIsClient(true);
   }, []);
 
   useEffect(() => {
-    if (allWords && allWords.length > 0) {
-      const shuffled = allWords.sort(() => 0.5 - Math.random());
-      setRandomWords(shuffled.slice(0, 5));
-    }
-  }, [allWords]);
+    words.setRandomWords(getRandomWords());
+  }, [getRandomWords]);
 
-  const refreshWords = () => {
-    if (allWords && allWords.length > 0) {
-      const shuffled = allWords.sort(() => 0.5 - Math.random());
-      setRandomWords(shuffled.slice(0, 5));
-      inputWords.words.clear();
-    }
-  };
+  const refreshWords = useCallback(() => {
+    words.setRandomWords(getRandomWords());
+  }, [getRandomWords]);
 
   return (
     <main className="flex flex-col space-y-2">
       <ul className="space-y-2">
         {isClient &&
-          randomWords.map(([word, translation]) => {
-            const inputValue = inputWords.words.get(word) || "";
+          Array.from(words.randomWords).map(([word, translation]) => {
+            const inputValue = words.pendingWords.get(word) || "";
 
             return (
               <li key={word} className="flex items-center space-x-2">
                 <strong className="w-xs text-right">{translation}:</strong>
-                <Input type="text" id={word} onChange={(e) => inputWords.set(word, e.target.value.toLowerCase())} value={inputValue} />
+                <Input type="text" id={word} onChange={(e) => words.setPendingWord(word, e.target.value.toLowerCase())} value={inputValue} />
                 <span title={word}>{inputValue === word ? "✅" : "❌"}</span>
               </li>
             );
           })}
       </ul>
       <div className="flex space-x-2 justify-end">
-        <Button onClick={refreshWords}>Refresh</Button>
+        <Button onClick={refreshWords} disabled={!words.correct}>
+          Refresh
+        </Button>
         <Link href="/add-word">
           <Button type="button">Add New Word</Button>
         </Link>
